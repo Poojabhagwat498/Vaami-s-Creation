5adc31063f526065bf6ece9f4427ac2aed2b10b6
Poojabhagwat498 <poojabhagwat2525@gmail.com>
2026-03-15 22:32:30 +0530

    aadd changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/',   // Should be '/' not '/some-subfolder/'
   plugins: [react(),tailwindcss()],
   server: {
     proxy: {
