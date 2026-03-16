eef348467289c8ecaf36823500efac4fb846b7be
Poojabhagwat498 <poojabhagwat2525@gmail.com>
2026-03-16 17:18:22 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
+
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
+app.get("/", (req, res) => {
+  res.send("Vaami's Creation Backend API is running 🚀");
+});
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
